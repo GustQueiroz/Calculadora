7b553beb60e45f23d1c129d8a0b8b1d3b9629a53
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2023-12-25 19:44:04 -0300

    funções de calculo completas

--- script.js
+++ script.js
@@ -69,9 +69,42 @@ class Calculator {
     this.clear();
   }
 
+  calculate() {
+    let result;
+
+    const _previousOperand = parseFloat(this.previousOperand);
+    const _currentOperand = parseFloat(this.currentOperand);
+
+    if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+    switch (this.operation) {
+      case "+":
+        result = _previousOperand + _currentOperand;
+        break;
+      case "-":
+        result = _previousOperand - _currentOperand;
+        break;
+      case "÷":
+        result = _previousOperand / _currentOperand;
+        break;
+      case "*":
+        result = _previousOperand * _currentOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
   chooseOperation(operation) {
+    if (this.previousOperand != "") {
+      this.calculate();
+    }
+
     this.operation = operation;
-    this.previousOperand = `${this.currentOperand} ${this.operation}`;
+    this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
 
@@ -86,7 +119,9 @@ class Calculator {
     this.operation = undefined;
   }
   updateDisplay() {
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.previousOperandTextElement.innerText = `${this.previousOperand} ${
+      this.operation || ""
+    }`;
     this.currentOperandTextElement.innerText = this.currentOperand;
   }
 }
@@ -112,5 +147,10 @@ for (const operationButton of operationButtons) {
 
 allClearButton.addEventListener("click", () => {
   calculator.clear();
+  calculator.updateDisplay();
+});
+
+equalsButton.addEventListener("click", () => {
+  calculator.calculate();
   calculator.updateDisplay();
 });
