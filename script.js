730f246e0bd946f1f1038b49562c03ee4a1b8c0b
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2023-12-25 18:46:21 -0300

    temas completos

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.querySelector('.ball').addEventListener('click', (e) => {
+document.querySelector('.themeButton').addEventListener('click', (e) => {
     const ballElement = document.getElementById('ball');
     const resultsElement = document.getElementById('results');
     const buttonContainerElement = document.getElementById('buttonContainer');
     const labelTextElement = document.getElementById('labelText');
+    const calcElement = document.getElementById('calc');
+    const themeTextElement = document.getElementById('themeText');
     
     if (ballElement.classList.contains('ball-move')) {
         ballElement.classList.remove('ball-move');
@@ -13,6 +15,10 @@ document.querySelector('.ball').addEventListener('click', (e) => {
         buttonContainerElement.classList.add('buttonContainerPurple');
         labelTextElement.classList.remove('labelTextWhite');
         labelTextElement.classList.add('labelTextPurple');
+        calcElement.classList.remove('calcWhite');
+        calcElement.classList.add('calcPurple');
+        themeTextElement.classList.remove('themeTextWhite');
+        themeTextElement.classList.add('themeTextPurple');
         document.body.classList.remove('white');
         document.body.classList.add('purple');
     } else if (ballElement.classList.contains('ball-move2')) {
@@ -23,6 +29,10 @@ document.querySelector('.ball').addEventListener('click', (e) => {
         buttonContainerElement.classList.add('buttonContainer');
         labelTextElement.classList.remove('labelTextPurple');
         labelTextElement.classList.add('labelText');
+        calcElement.classList.remove('calcPurple');
+        calcElement.classList.add('calc');
+        themeTextElement.classList.remove('themeTextPurple');
+        themeTextElement.classList.add('themeText');
         document.body.classList.remove('purple');
     } else {
         ballElement.classList.add('ball-move');
@@ -30,8 +40,12 @@ document.querySelector('.ball').addEventListener('click', (e) => {
         resultsElement.classList.add('resultsWhite');
         buttonContainerElement.classList.remove('buttonContainer');
         buttonContainerElement.classList.add('buttonContainerWhite');
-        labelTextElement.classList.add('labelText');
+        labelTextElement.classList.remove('labelText');
         labelTextElement.classList.add('labelTextWhite');
+        calcElement.classList.remove('calc');
+        calcElement.classList.add('calcWhite');
+        themeTextElement.classList.remove('themeText');
+        themeTextElement.classList.add('themeTextWhite');
         document.body.classList.add('white');
     }
 });
